393049f810369059e905d8dcd470bcf4195f63a6
toby <rtprafke96@gmail.com>
2020-08-05 20:20:55 -0400

    Event-listeners

--- script.js
+++ script.js
@@ -5,40 +5,42 @@ let expenseContainer = document.querySelector(".expense-container");
 let expenses = document.querySelector(".expenses");
 let remainingParagraph = document.querySelector(".remaining");
 
-weekly.addEventListener("submit", (e) => {
-  e.preventDefault();
-  expenseContainer.innerHTML = "";
-  let data = new FormData(weekly);
-  let income = data.get("amount");
-  let remaining = income;
-  console.log(remaining);
+let remaining = 0;
 
-  remainingParagraph.innerText = `Remaining: ${remaining}`;
-  expenses.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let data2 = new FormData(expenses);
-    let expense = data2.get("category");
-    let number = data2.get("amount");
-    remaining = remaining - number;
-    console.log(remaining);
-    remainingParagraph.innerText = `Remaining: ${remaining}`;
-    if (expense === "entertainment") {
-      let print = document.createElement("div");
-      print.innerText = ` Entertainment -$${number}`;
-      expenseContainer.append(print);
-    } else if (expense === "food") {
-      let print = document.createElement("div");
-      print.innerText = ` Grub -$${number}`;
-      expenseContainer.append(print);
-    } else if (expense === "clothing") {
-      let print = document.createElement("div");
-      print.innerText = ` Drip -$${number}`;
-      expenseContainer.append(print);
-    } else {
-      let print = document.createElement("div");
-      print.innerText = `Bills -$${number}`;
-      expenseContainer.append(print);
-    }
-  });
-  weekly.reset();
+weekly.addEventListener("submit", (e) => {
+	e.preventDefault();
+	expenseContainer.innerHTML = "";
+	let data = new FormData(weekly);
+	let income = data.get("amount");
+	remaining = income;
+	console.log(remaining);
+
+	remainingParagraph.innerText = `Remaining: ${remaining}`;
+});
+
+expenses.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let data2 = new FormData(expenses);
+	let expense = data2.get("category");
+	let number = data2.get("amount");
+	remaining = remaining - number;
+	console.log(remaining);
+	remainingParagraph.innerText = `Remaining: ${remaining}`;
+	if (expense === "entertainment") {
+		let print = document.createElement("div");
+		print.innerText = ` Entertainment -$${number}`;
+		expenseContainer.append(print);
+	} else if (expense === "food") {
+		let print = document.createElement("div");
+		print.innerText = ` Grub -$${number}`;
+		expenseContainer.append(print);
+	} else if (expense === "clothing") {
+		let print = document.createElement("div");
+		print.innerText = ` Drip -$${number}`;
+		expenseContainer.append(print);
+	} else {
+		let print = document.createElement("div");
+		print.innerText = `Bills -$${number}`;
+		expenseContainer.append(print);
+	}
 });
