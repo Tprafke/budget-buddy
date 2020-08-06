b00699d26945899361dd67e4e56a22876bcf2e5c
Abdulvip <ahamdidueen@outlook.com>
2020-08-06 14:31:50 -0400

    me again

--- script.js
+++ script.js
@@ -11,41 +11,47 @@ let clothingContainer = document.querySelector(".clothing-container");
 let remaining = 0;
 
 weekly.addEventListener("submit", (e) => {
-	e.preventDefault();
-	let data = new FormData(weekly);
-	let income = data.get("amount");
-	remaining = income;
-	entertainmentContainer.innerHTML = "";
-	foodContainer.innerHTML = "";
-	billsContainer.innerHTML = "";
-	clothingContainer.innerHTML = "";
-	remainingParagraph.innerText = `Remaining: ${remaining}`;
+  e.preventDefault();
+  let data = new FormData(weekly);
+  let income = data.get("amount");
+  remaining = income;
+  entertainmentContainer.innerHTML = "";
+  foodContainer.innerHTML = "";
+  billsContainer.innerHTML = "";
+  clothingContainer.innerHTML = "";
+  remainingParagraph.innerText = `Remaining: ${remaining}`;
+  remainingParagraph.classList.remove("maroon");
 });
 
 expenses.addEventListener("submit", (e) => {
-	e.preventDefault();
-	let data2 = new FormData(expenses);
-	let expense = data2.get("category");
-	let number = data2.get("amount");
-	let expenseType = data2.get("expense-type");
-	remaining = remaining - number; 
-	let someVariable = document.createElement("p");
-	remainingParagraph.innerText = `Remaining: $${remaining}`;
-	if (expense === "entertainment") {
-		//  entertainmentContainer.innerText = "Entertainment";
-		someVariable.innerText = ` ${expenseType} -$${number}`;
-		entertainmentContainer.append(someVariable);
-	} else if (expense === "food") {
-		// foodContainer.innerText = `Food`;
-		someVariable.innerText = ` ${expenseType} -$${number}`;
-		foodContainer.append(someVariable);
-	} else if (expense === "clothing") {
-		// clothingContainer.innerText = `Clothing`;
-		someVariable.innerText = ` ${expenseType} -$${number}`;
-		clothingContainer.append(someVariable);
-	} else {
-		// billsContainer.innerText = `Bills`;
-		someVariable.innerText = `${expenseType} -$${number}`;
-		billsContainer.append(someVariable);
-	}
+  e.preventDefault();
+  let data2 = new FormData(expenses);
+  let expense = data2.get("category");
+  let number = data2.get("amount");
+  let expenseType = data2.get("expense-type");
+  remaining = remaining - number;
+  let someVariable = document.createElement("p");
+  remainingParagraph.innerText = `Remaining: $${remaining}`;
+  if (expense === "entertainment") {
+    //  entertainmentContainer.innerText = "Entertainment";
+    someVariable.innerText = ` ${expenseType} -$${number}`;
+    entertainmentContainer.append(someVariable);
+  } else if (expense === "food") {
+    // foodContainer.innerText = `Food`;
+    someVariable.innerText = ` ${expenseType} -$${number}`;
+    foodContainer.append(someVariable);
+  } else if (expense === "clothing") {
+    // clothingContainer.innerText = `Clothing`;
+    someVariable.innerText = ` ${expenseType} -$${number}`;
+    clothingContainer.append(someVariable);
+  } else {
+    // billsContainer.innerText = `Bills`;
+    someVariable.innerText = `${expenseType} -$${number}`;
+    billsContainer.append(someVariable);
+  }
+  if (remaining < 0) {
+    remainingParagraph.classList.add("maroon");
+  } else {
+    remainingParagraph.classList.remove("maroon");
+  }
 });
